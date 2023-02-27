e9853a2fa949d5b53b688b09b8d5b93e71f00d4c
Henry Gilbert <77060624+henrygilbert22@users.noreply.github.com>
2023-02-26 19:25:55 -0600

    saving

--- chatgpt_util.py
+++ chatgpt_util.py
@@ -3,6 +3,7 @@ import openai
 import os 
 from typing import List
 import numpy as np
+import backoff 
 
 class ChatGPTUtil:
     
@@ -15,6 +16,7 @@ class ChatGPTUtil:
         return wrapper
 
     @classmethod
+    @backoff.on_exception(backoff.expo, openai.error.RateLimitError)
     @set_api_key
     def get_text_response(cls, prompt: str, engine: str = 'ada', max_tokens: int=1000, samples: int=1) -> List[str]:
         response = openai.Completion.create(
@@ -26,9 +28,10 @@ class ChatGPTUtil:
             raise ValueError(f'No choices in response: {response}')
         return [choice['text'] for choice in response['choices']]
     
-    @classmethod
+    @classmethod        
+    @backoff.on_exception(backoff.constant, openai.error.RateLimitError, interval=30, jitter=None)
     @set_api_key
-    def get_text_embedding(cls, input: str, model: str = 'text-embedding-ada-002') -> np.ndarray:
+    def get_text_embedding(cls, input: str, model: str = 'text-embedding-ada-002') -> List[float]:
         response = openai.Embedding.create(
             model=model,
             input=input)
@@ -38,5 +41,5 @@ class ChatGPTUtil:
             raise ValueError(f'More than one embedding in response: {response}')
         elif 'embedding' not in response['data'][0]:
             raise ValueError(f'No embedding in response: {response}')
-        return np.array(response['data'][0]['embedding'])
+        return response['data'][0]['embedding']
        
